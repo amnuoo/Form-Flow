63678a6a53538493634d8ab3d49b5635df250b0f
amnuoo <ameenahsan.blvk@gmail.com>
2026-02-17 10:08:35 +0000

    initial commit

---
+++ formflow/formflow/doctype/form_configuration/form_configuration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Ameen and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Form Configuration", {
+// 	refresh(frm) {
+
+// 	},
+// });
