8fc16c54ebe7c28cd4bea4e9c7318072351f7316
amnuoo <ameenahsan.blvk@gmail.com>
2026-02-19 05:11:05 +0000

    feat: added login validation for public form access and checked the activation status of form

---
+++ formflow/formflow/api.py
@@ -0,0 +1,20 @@
+import frappe
+from .utils import generate_unique_id
+
+
+@frappe.whitelist(allow_guest=True)
+def get_form_config(form_name):
+
+    form = frappe.get_doc(
+        "Form Configuration",
+        {"form_name": form_name}
+    )
+
+    if not form.is_active:
+        frappe.throw("Form is inactive")
+
+    if form.require_login and frappe.session.user == "Guest":
+        frappe.throw("Login required to access this form")
+
+    return form
+
