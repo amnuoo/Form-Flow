9d017e976138b9ec83ddcf6fb29e1bcebd3eb3f6
amnuoo <ameenahsan.blvk@gmail.com>
2026-03-03 06:18:36 +0000

    feat:fetches fields of target doctype to the form configuration

--- formflow/formflow/api.py
+++ formflow/formflow/api.py
@@ -32,7 +32,6 @@ def get_form_config(form_name):
             "fieldtype": df.fieldtype
         }
 
-        # Child table support
         if df.fieldtype == "Table":
             child_meta = frappe.get_meta(df.options)
             field_data["child_fields"] = [
@@ -55,6 +54,36 @@ def get_form_config(form_name):
 
 
 @frappe.whitelist(allow_guest=True)
+def get_doc_by_unique_id(form_name, unique_id):
+
+    form = frappe.get_doc("Form Configuration", {"form_name": form_name})
+
+    if not form.is_active:
+        frappe.throw("Form is inactive")
+
+    if form.require_login and frappe.session.user == "Guest":
+        frappe.throw("Login required")
+
+    doc_name = frappe.db.get_value(
+        form.target_doctype,
+        {form.unique_id_field: unique_id}
+    )
+
+    if not doc_name:
+        frappe.throw("Invalid Reference ID")
+
+    doc = frappe.get_doc(form.target_doctype, doc_name)
+
+    allowed_fields = [f.fieldname for f in form.form_fields if not f.hidden]
+
+    data = {}
+    for field in allowed_fields:
+        data[field] = doc.get(field)
+
+    return data
+
+
+@frappe.whitelist(allow_guest=True)
 def submit_form(form_name, data, unique_id=None):
 
     data = frappe.parse_json(data)
@@ -73,7 +102,9 @@ def submit_form(form_name, data, unique_id=None):
         if field.required and not cleaned_data.get(field.fieldname):
             frappe.throw(f"{field.fieldname} is mandatory")
 
-  
+    action_type = ""
+    new_id = ""
+
     if not unique_id:
 
         if not form.allow_create:
@@ -90,11 +121,12 @@ def submit_form(form_name, data, unique_id=None):
         doc.set(form.unique_id_field, new_id)
         doc.insert(ignore_permissions=True)
 
+        action_type = "Create"
 
     else:
 
         if not form.allow_update:
-            frappe.throw("Form update is not allowed")
+            frappe.throw("update is not allowed")
 
         doc_name = frappe.db.get_value(
             form.target_doctype,
@@ -109,5 +141,65 @@ def submit_form(form_name, data, unique_id=None):
         doc.save(ignore_permissions=True)
 
         new_id = unique_id
+        action_type = "Update"
+
+    meta = frappe.get_meta(form.target_doctype)
+
+    for field in form.form_fields:
+        df = meta.get_field(field.fieldname)
+        if df and df.fieldtype == "Attach":
+            file_url = cleaned_data.get(field.fieldname)
+            if file_url:
+                frappe.db.set_value(
+                    "File",
+                    {"file_url": file_url},
+                    {
+                        "attached_to_doctype": form.target_doctype,
+                        "attached_to_name": doc.name
+                    }
+                )
+
+    try:
+        ip_address = frappe.local.request_ip or "Unknown"
+
+        log_doc = frappe.get_doc({
+            "doctype": "Form Submission Log",
+            "form": form_name,
+            "unique_id": new_id,
+            "ip_address": ip_address,
+            "action": action_type,
+            "timestamp": frappe.utils.now()
+        })
+
+        log_doc.insert(ignore_permissions=True)
+
+    except Exception:
+        pass
 
     return {"unique_id": new_id}
+
+
+@frappe.whitelist(allow_guest=True)
+def upload_public_file():
+
+    from frappe.utils.file_manager import save_file
+
+    if "file" not in frappe.request.files:
+        frappe.throw("No file attached")
+
+    uploaded_file = frappe.request.files["file"]
+    content = uploaded_file.stream.read()
+
+    file_doc = save_file(
+        uploaded_file.filename,
+        content,
+        None,
+        None,
+        is_private=0
+    )
+
+    return {
+        "file_url": file_doc.file_url
+    }
+
+
--- formflow/formflow/doctype/form_configuration/form_configuration.js
+++ formflow/formflow/doctype/form_configuration/form_configuration.js
@@ -1,8 +1,49 @@
-// Copyright (c) 2026, Ameen and contributors
-// For license information, please see license.txt
+frappe.ui.form.on('Form Configuration', {
 
-// frappe.ui.form.on("Form Configuration", {
-// 	refresh(frm) {
+    refresh(frm) {
+        load_doctype_fields(frm);
+    },
 
-// 	},
-// });
+    target_doctype(frm) {
+        load_doctype_fields(frm);
+    }
+
+});
+
+function load_doctype_fields(frm) {
+
+    if (!frm.doc.target_doctype) return;
+
+    frappe.call({
+        method: "frappe.client.get",
+        args: {
+            doctype: "DocType",
+            name: frm.doc.target_doctype
+        },
+        callback: function(r) {
+
+            if (!r.message) return;
+
+            let fields = r.message.fields
+                .filter(df =>
+                    !["Section Break", "Column Break"].includes(df.fieldtype)
+                )
+                .map(df => df.fieldname);
+
+            // Convert Data field into Select temporarily
+            frm.fields_dict.form_fields.grid.update_docfield_property(
+                "fieldname",
+                "fieldtype",
+                "Select"
+            );
+
+            frm.fields_dict.form_fields.grid.update_docfield_property(
+                "fieldname",
+                "options",
+                fields.join("\n")
+            );
+
+            frm.refresh_field("form_fields");
+        }
+    });
+}
